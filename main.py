c09a396d24910746f46fe151b803824f16add4dc
shon <ducsonds2005@gmail.com>
2024-12-10 01:30:33 +0700

    Initial commit for second remote

--- main.py
+++ main.py
@@ -1,58 +1,63 @@
-import gym_cutting_stock
 import gymnasium as gym
+import gym_cutting_stock
 from policy import GreedyPolicy, RandomPolicy
 from student_submissions.s2210xxx.policy2210xxx import Policy2210xxx
 
+import numpy as np
+import time
 # Create the environment
 env = gym.make(
     "gym_cutting_stock/CuttingStock-v0",
     render_mode="human",  # Comment this line to disable rendering
 )
-NUM_EPISODES = 100
+NUM_EPISODES = 2
 
 if __name__ == "__main__":
     # Reset the environment
-    observation, info = env.reset(seed=42)
+    # observation, info = env.reset(seed=42)
+    # #print(info)
+    # print(observation["stocks"])
+    # # Test GreedyPolicy
+    # gd_policy = GreedyPolicy()
+    # ep = 0
+    # while ep < NUM_EPISODES:
+    #     action = gd_policy.get_action(observation, info)
+    #     observation, reward, terminated, truncated, info = env.step(action)
+    #     print(ep, ": ", info)
+    #     print()
+    #     if terminated or truncated:
+    #         print(info)
+    #         observation, info = env.reset(seed=ep)
+    #         ep += 1
 
-    # Test GreedyPolicy
-    gd_policy = GreedyPolicy()
-    ep = 0
-    while ep < NUM_EPISODES:
-        action = gd_policy.get_action(observation, info)
-        observation, reward, terminated, truncated, info = env.step(action)
-
-        if terminated or truncated:
-            print(info)
-            observation, info = env.reset(seed=ep)
-            ep += 1
-
-    # Reset the environment
-    observation, info = env.reset(seed=42)
-
-    # Test RandomPolicy
-    rd_policy = RandomPolicy()
-    ep = 0
-    while ep < NUM_EPISODES:
-        action = rd_policy.get_action(observation, info)
-        observation, reward, terminated, truncated, info = env.step(action)
-
-        if terminated or truncated:
-            print(info)
-            observation, info = env.reset(seed=ep)
-            ep += 1
-
-    # Uncomment the following code to test your policy
     # # Reset the environment
     # observation, info = env.reset(seed=42)
-    # print(info)
 
-    # policy2210xxx = Policy2210xxx(policy_id=1)
-    # for _ in range(200):
-    #     action = policy2210xxx.get_action(observation, info)
+    # # Test RandomPolicy
+    # rd_policy = RandomPolicy()
+    # ep = 0
+    # while ep < NUM_EPISODES:
+    #     action = rd_policy.get_action(observation, info)
     #     observation, reward, terminated, truncated, info = env.step(action)
-    #     print(info)
-
+    #     print(ep, ": ", info)
+    #     print()
     #     if terminated or truncated:
-    #         observation, info = env.reset()
+    #         print(info)
+    #         observation, info = env.reset(seed=ep)
+    #         ep += 1
+
+    #Uncomment the following code to test your policy
+    # Reset the environment
+    observation, info = env.reset(seed=42)
+    policy2210xxx = Policy2210xxx(policy_id=1)
+    for _ in range(200):
+        action = policy2210xxx.get_action(observation, info)
+        observation, reward, terminated, truncated, info = env.step(action)
+        print()
+        print(_, "st loop: ", info)
+        #print(action)
+        time.sleep(0.25)
+        if terminated or truncated:
+            observation, info = env.reset()
 
 env.close()
--- student_submissions/s2210xxx/policy2210xxx.py
+++ student_submissions/s2210xxx/policy2210xxx.py
@@ -1,19 +1,106 @@
 from policy import Policy
+import numpy as np
+from scipy.optimize import linprog
 
-
-def Policy2210xxx(Policy):
+#Width is the number of elements with -1 in one column, which means that the width is vertical, axis = 1
+#height is the number of elements with -1 in one row, which means that the height is horizontal, axis = 0
+class Policy2210xxx(Policy):
     def __init__(self, policy_id=1):
         assert policy_id in [1, 2], "Policy ID must be 1 or 2"
-
-        # Student code here
-        if policy_id == 1:
-            pass
-        elif policy_id == 2:
-            pass
+        self.policy_id = policy_id
 
     def get_action(self, observation, info):
-        # Student code here
-        pass
+        if self.policy_id == 1:
+            #print("Products information before sorting: ", observation["products"])
+            
+            # Sort the products by size in descending order.
+            # sorted() function is a built-in function for sorting. For more information see the documentation.
+            products = sorted(observation["products"], key=lambda x: x["size"][0] * x["size"][1], reverse=True)
+            #print("Products information after sorting: ", products)
+            
+            #Take out the information about available stocks
+            stocks = observation["stocks"]
+            
+            # Iterate through the products
+            for product in products:
+                
+                # If the quantity of current product is greater than 0,
+                # continue to insert it into the stocks
+                if product["quantity"] > 0:
+                    
+                    # Get the size of the current product
+                    prod_size = product["size"]
+                    
+                    # Iterate through all available stocks.
+                    # Iterate using enumerate() to get both the index and the stock,
+                    # See the document for information.
+                    for stock_idx, stock in enumerate(stocks):
+                        
+                        # Get the size of the current stock
+                        stock_w, stock_h = self._get_stock_size_(stock)
+                        
+                        # Get the size of the current product into two variables
+                        prod_w, prod_h = prod_size
 
-    # Student code here
-    # You can add more functions if needed
+                        # Initial condition: The size of the product must be less than or equal to the size of the stock.
+                        if stock_w >= prod_w and stock_h >= prod_h:
+                            
+                            # Iterate through all possible indexes in the x coordinate where we can put 
+                            # the top-left corner of the product.
+                            for x in range(stock_w - prod_w + 1):
+                                
+                                # Inner loop to iterate through all possible indexes in the y coordinate where we can put
+                                # the bootom-right corner of the product.
+                                for y in range(stock_h - prod_h + 1):
+                                    
+                                    # Check if the product can be placed at the current position
+                                    # If yes, return the information for the environment to place the product.
+                                    if self._can_place_(stock, (x, y), prod_size):
+                                        
+                                        # Necessary datas for the environment to place the product,
+                                        # including: the index of the stock that we insert the new product,
+                                        # the size of the product, and the position of the top-left corner of the product.
+                                        return {"stock_idx": stock_idx, "size": prod_size, "position": (x, y)}
+
+                        # else, rotate the product and check if it can fit into the stock
+                        if stock_w >= prod_h and stock_h >= prod_w:
+                            for x in range(stock_w - prod_h + 1):
+                                for y in range(stock_h - prod_w + 1):
+                                    
+                                    # The slicing notation [::-1] is used to reverse the order of the elements in the list,
+                                    # which means that the width and height of the product size are swapped.
+                                    if self._can_place_(stock, (x, y), prod_size[::-1]):
+                                        return {"stock_idx": stock_idx, "size": prod_size[::-1], "position": (x, y)}
+
+            # If no valid position is found, return a dummy action
+            return {"stock_idx": 0, "size": [0, 0], "position": (0, 0)}
+        
+        elif self.policy_id == 2:
+            pass
+
+    def _get_stock_size_(self, stock):
+        stock_w = np.sum(np.any(stock != -2, axis=1))
+        stock_h = np.sum(np.any(stock != -2, axis=0))
+        return stock_w, stock_h
+
+    def _can_place_(self, stock, position, size):
+        # Check if the product can be placed at the given position
+        
+        # Get the x and y coordinates of the top-left corner of the product
+        pos_x, pos_y = position
+        
+        # Get the width and height of the product
+        width, height = size
+        
+        # As the "stocks" key of the observation is a list of 2D numpy arrays,
+        # we check using the dimension of the stock and the position of the product
+        if pos_x + width <= stock.shape[0] and pos_y + height <= stock.shape[1]:
+            if np.all(stock[pos_x:pos_x + width, pos_y:pos_y + height] == -1):
+                return True
+        return False
+
+    def calculate_available_space(self, stock):
+        # Count the number of -1 elements in the stock
+        available_space = np.sum(stock == -1)
+        return available_space
+    
